170796110d338e3603a6039cdd4d0848a618afe7
gautier59 <gautier.riviere@altran.com>
2017-02-22 16:59:49 +0100

    add view in bootom of historyFragment

--- app/src/main/java/com/example/gauti/cobra/fragments/history/HistoryFragment.java
+++ app/src/main/java/com/example/gauti/cobra/fragments/history/HistoryFragment.java
@@ -3,31 +3,56 @@ package com.example.gauti.cobra.fragments.history;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 
 import com.example.gauti.cobra.R;
 import com.example.gauti.cobra.event.EventAlerte;
 import com.example.gauti.cobra.fragments.CobraFragment;
 import com.example.gauti.cobra.model.Alerte;
 import com.example.gauti.cobra.provider.AlerteProvider;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 
 import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.OnItemClick;
 import de.greenrobot.event.EventBus;
+
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
 
 public class HistoryFragment extends CobraFragment {
 
+    // Private fields
+    // --------------------------------------------------------------------------------------------
     private HistoryAdapterMenu historyAdapterMenu;
+    private GoogleMap googleMap;
+    private MapView mapView;
 
-    @Bind(R.id.lvHistory)
+    // Views
+    // --------------------------------------------------------------------------------------------
+    @Bind(R.id.lvHistoryMenu)
     ListView lvHistory;
 
+    @Bind(R.id.rlMapHostoryMenu)
+    RelativeLayout rlMapHostoryMenu;
+
+    @Bind(R.id.btn_delete_all)
+    Button btnDeleteAll;
+
+    // Life cycle
+    // --------------------------------------------------------------------------------------------
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -44,6 +69,26 @@ public class HistoryFragment extends CobraFragment {
     public void onDestroy() {
         super.onDestroy();
         EventBus.getDefault().unregister(this);
+    }
+
+    @OnItemClick(R.id.lvHistoryMenu)
+    void onItemClickHistoryMenu(int position) {
+        mapVisible(true);
+    }
+
+    @OnClick(R.id.ivExit)
+    void onClickExitMap() {
+        mapVisible(false);
+    }
+
+    private void mapVisible(boolean b) {
+        if (b) {
+            rlMapHostoryMenu.setVisibility(View.VISIBLE);
+            btnDeleteAll.setVisibility(View.GONE);
+        } else {
+            rlMapHostoryMenu.setVisibility(View.GONE);
+            btnDeleteAll.setVisibility(View.VISIBLE);
+        }
     }
 
     @OnClick(R.id.btn_delete_all)
